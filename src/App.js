f6899028b5aba20c7f59d5e13d73b2f5aa177754
Gianluca Casati <io@iMac-di-Gianluca.local>
2022-10-24 11:40:13 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,21 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+import { ConnectButton, Web3Modal } from "@web3modal/react";
+
+const config = {
+  projectId: process.env.REACT_APP_WALLETCONNECT_PROJECTID,
+  theme: "dark",
+  accentColor: "default",
+  ethereum: {
+    appName: process.env.REACT_APP_WALLETCONNECT_PROJECTNAME,
+  },
+};
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <ConnectButton />
+        <Web3Modal config={config} />
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/WalletConnect/web3modal"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          WalletConnect/web3modal
         </a>
       </header>
     </div>
